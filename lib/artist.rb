344272ddfbc1c1e3878b0fd69475c5faa7fd29fd
jessicashannon <jessdahling@gmail.com>
2015-08-05 14:43:02 -0400

    this doesn't work yet

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,3 +1,10 @@
 class Artist
-  # code goes here
+attr_accessor :name
+attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+
+
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,24 @@
+require 'tempfile'
+
 class Song
-  # code goes here
+  attr_accessor :title
+  attr_reader :title, :artist
+
+  def artist
+    @name
+  end
+
+  def artist=(name)
+    @name = name
+  end
+
+  def serialize
+    sanitized_name = artist.name.downcase.split.join('_')
+    file = Tempfile.new(sanitized_name + '.txt', 'tmp')
+  file.write("#{artist.name} - #{title}")
+  file.rewind
+  file.close
+  file.unlink
+  end  
+
 end
